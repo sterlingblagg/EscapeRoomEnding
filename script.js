0caf74d8ca8096cede448ef908e0c1405d77933c
Sterling Blagg <37843170+sterlingblagg@users.noreply.github.com>
2019-03-12 12:13:21 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ a = document.getElementById("button");
 
 function buttonPageSwap(el){
 	if (a.src.match('restart')){ 
-		document.location.href = "https://www.google.com"
+		document.location.href = "https://malakhyeshine.github.io/escaperoom/Phase8/escaperoom.html"
 	} else {
 	
 	}

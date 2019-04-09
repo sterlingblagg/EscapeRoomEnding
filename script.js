ff05b848190edf4ed33fb652f4713a6408bc3188
Sterling Blagg <37843170+sterlingblagg@users.noreply.github.com>
2019-04-09 15:06:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ a = document.getElementById("button");
 
 function buttonPageSwap(el){
 	if (a.src.match('restart')){ 
-		document.location.href = "https://malakhyeshine.github.io/escaperoom/Phase8/escaperoom.html"
+		document.location.href = "https://malakhyeshine.github.io/escaperoom/Final%20copy/escaperoom.html"
 	} else {
 	
 	}
